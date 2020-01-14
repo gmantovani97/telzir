6c627a1b39f6300c91ba8f99aa9266c5575ca5c6
Giovani Mantovani <giovanimantovani@MacBook-Pro.local>
2020-01-14 08:46:21 -0300

    Configuration about colors and metrics

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,5 +3,23 @@ import React from 'react';
 import './styles.scss';
 
 export default function Header() {
-  return <div />;
+  return (
+    <div className="container">
+      <h1>FaleMais</h1>
+      <div className="menu-container">
+        <div className="menu-button">
+          <p>Home</p>
+        </div>
+        <div className="menu-button">
+          <p>Pricing</p>
+        </div>
+        <div className="menu-button">
+          <p>About us</p>
+        </div>
+        <div className="get-started-button">
+          <p>Get Started</p>
+        </div>
+      </div>
+    </div>
+  );
 }
